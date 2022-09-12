32384191dc01a418b2fdfddec72ed8fd83fb93a6
zhuhe <zhuhe.cn>
2022-09-12 10:53:28 +0800

    Initial commit

---
+++ src/main/java/com/zhuhe/secondskill/SecondskillApplication.java
@@ -0,0 +1,13 @@
+package com.zhuhe.secondskill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondskillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondskillApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhuhe/secondskill/SecondskillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhuhe.secondskill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondskillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
